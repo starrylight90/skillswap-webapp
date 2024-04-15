419df4f1f35da04af14d0ea91ff7f3b3e499fe4a
Vedanshi Sahu <92024144+11roxanne@users.noreply.github.com>
2024-04-15 20:39:20 +0530

    ui updation (#4)
    * Added AboutUs page
    
    * Added AboutUs page
    
    * Frontend update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,6 @@ import Login from './pages/Login/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Footer'; // Import Footer component from its file
 import MyNavbar from './MyNavbar'; // Import MyNavbar component from its file
-import Match from './pages/Matches/Match';
-import UserProfile from './pages/userProfile/userProfile';
 
 const App = () => {
   return (
@@ -19,8 +17,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/match" element={<Match />} />
-          <Route path="/user" element={<UserProfile/>} />
+          
         </Routes>
         <Footer />
       </div>
